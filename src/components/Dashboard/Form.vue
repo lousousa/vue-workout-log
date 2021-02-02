7ad876b7bb8948ff27a05ca67eb02d41ce313949
wizard <rumpertumpski@gmail.com>
2021-02-02 11:55:19 -0300

    add form component

---
+++ src/components/Dashboard/Form.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div
+        form
+            h1.text-center.text-xs.text-gray-400 INSERT AN ITEM
+            .flex.flex-wrap.items-end
+                .p-1(class='w-1/4')
+                    label.text-gray-600.text-xs Minutes spent
+                    input.border.w-full.rounded.p-1
+                .p-1(class='w-1/4')
+                    label.text-gray-600.text-xs Activity
+                    input.border.w-full.rounded.p-1
+                .p-1(class='w-1/4')
+                    label.text-gray-600.text-xs Date
+                    input.border.w-full.rounded.p-1
+                .p-1(class='w-1/4')
+                    button.text-xs.w-full.bg-turquoise-100.rounded.p-2.border.border-turquoise-200.text-turquoise-500(type='submit') Add
+</template>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,4 +1,14 @@
 <template lang='pug'>
-    div
-        h1 welcome!
+    .max-w-2xl.mx-auto.my-4.border.rounded.bg-white
+        .p-4.border-b
+            h1.font-bold.text-sm.text-gray-600.text-center Workout Log
+        .p-4
+            Form
 </template>
+
+<script>
+    import Form from './Dashboard/Form'
+    export default {
+        components: { Form }
+    }
+</script>
