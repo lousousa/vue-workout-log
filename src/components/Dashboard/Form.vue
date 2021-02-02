47a21cb6be72c12399d89f79027437846999a0a7
wizard <rumpertumpski@gmail.com>
2021-02-02 12:09:46 -0300

    add form model

--- src/components/Dashboard/Form.vue
+++ src/components/Dashboard/Form.vue
@@ -1,17 +1,47 @@
 <template lang="pug">
     div
-        form
+        form(@submit='handleSubmit')
             h1.text-center.text-xs.text-gray-400 INSERT AN ITEM
             .flex.flex-wrap.items-end
                 .p-1(class='w-1/4')
-                    label.text-gray-600.text-xs Minutes spent
-                    input.border.w-full.rounded.p-1
+                    label.text-gray-600.text-xs Minutes spent:
+                    input.border.w-full.rounded.p-2.text-xs(v-model='model.minutes')
                 .p-1(class='w-1/4')
-                    label.text-gray-600.text-xs Activity
-                    input.border.w-full.rounded.p-1
+                    label.text-gray-600.text-xs Activity:
+                    select.border.w-full.rounded.p-2.text-xs(v-model='model.activity')
+                        option.text-xs(v-for='activity, idx in options.activities' :key='idx' :value='activity.value') {{ activity.label }}
                 .p-1(class='w-1/4')
-                    label.text-gray-600.text-xs Date
-                    input.border.w-full.rounded.p-1
+                    label.text-gray-600.text-xs Date:
+                    input.border.w-full.rounded.p-2.text-xs(v-model='model.date')
                 .p-1(class='w-1/4')
                     button.text-xs.w-full.bg-turquoise-100.rounded.p-2.border.border-turquoise-200.text-turquoise-500(type='submit') Add
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                model: {
+                    minutes: null,
+                    activity: null,
+                    date: null
+                },
+                options: {
+                    activities: [
+                        { label: 'Run', value: 'run' },
+                        { label: 'Swimming', value: 'swimming' },
+                        { label: 'Bike', value: 'bike' },
+                        { label: 'Surfing', value: 'surfing' },
+                        { label: 'Skate', value: 'skate' }
+                    ]
+                }
+            }
+        },
+        methods: {
+            handleSubmit (ev) {
+                ev.preventDefault()
+                console.log(this.model)
+            }
+        }
+    }
+</script>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,7 @@
 <template lang='pug'>
     .max-w-2xl.mx-auto.my-4.border.rounded.bg-white
         .p-4.border-b
-            h1.font-bold.text-sm.text-gray-600.text-center Workout Log
+            h1.font-bold.text-sm.text-center.text-turquoise-200 Workout Log
         .p-4
             Form
 </template>
