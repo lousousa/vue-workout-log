37c89676ac60972d782099aba0da2590ecc09d57
wizard <rumpertumpski@gmail.com>
2021-02-04 09:40:30 -0300

    add v-calendar

--- src/components/Dashboard/Form.vue
+++ src/components/Dashboard/Form.vue
@@ -5,20 +5,25 @@
             .flex.flex-wrap.items-end
                 .p-1(class='w-1/4')
                     label.text-gray-600.text-xs Date:
-                    input.border.w-full.rounded.p-2.text-xs(v-model='model.date')
+                    //input.border.w-full.rounded.p-2.text-xs(v-model='model.date')
+                    DatePicker(v-model='model.date')
+                        template(v-slot="{ inputValue, inputEvents }")
+                            input.w-full.p-2.border.rounded.text-xs(class='focus:outline-none focus:border-blue-300' :value='inputValue' v-on='inputEvents')
                 .p-1(class='w-1/4')
                     label.text-gray-600.text-xs Activity:
-                    select.border.w-full.rounded.p-2.text-xs(v-model='model.activity')
+                    select.border.w-full.rounded.p-2.text-xs(v-model='model.activity' class='focus:outline-none focus:border-blue-300')
                         option.text-xs(v-for='activity, idx in options.activities' :key='idx' :value='activity.value') {{ activity.label }}
                 .p-1(class='w-1/4')
                     label.text-gray-600.text-xs Minutes spent:
-                    input.border.w-full.rounded.p-2.text-xs(v-model='model.minutes')
+                    input.border.w-full.rounded.p-2.text-xs(class='focus:outline-none focus:border-blue-300' v-model='model.minutes')
                 .p-1(class='w-1/4')
-                    button.text-xs.w-full.bg-turquoise-100.rounded.p-2.border.border-turquoise-200.text-turquoise-500(type='submit') Add
+                    button.text-xs.w-full.bg-turquoise-100.rounded.p-2.border.border-turquoise-200.text-turquoise-500(class='focus:outline-none' type='submit') Add
 </template>
 
 <script>
+    import DatePicker from 'v-calendar/lib/components/date-picker.umd'
     export default {
+        components: { DatePicker },
         data () {
             return {
                 model: {
@@ -28,11 +33,11 @@
                 },
                 options: {
                     activities: [
-                        { label: 'Run', value: 'run' },
-                        { label: 'Swimming', value: 'swimming' },
-                        { label: 'Bike', value: 'bike' },
-                        { label: 'Surfing', value: 'surfing' },
-                        { label: 'Skate', value: 'skate' }
+                        { label: 'Run', value: 'Run' },
+                        { label: 'Swimming', value: 'Swimming' },
+                        { label: 'Bike', value: 'Bike' },
+                        { label: 'Surfing', value: 'Surfing' },
+                        { label: 'Skate', value: 'Skate' }
                     ]
                 }
             }
@@ -40,7 +45,10 @@
         methods: {
             handleSubmit (ev) {
                 ev.preventDefault()
-                this.$store.commit('addToLogs', Object.assign({}, this.model))
+                let entry = Object.assign({}, this.model)
+                entry.date = this.$root.moment(entry.date).format('DD/MM/YYYY')
+                console.log(entry)
+                // this.$store.commit('addToLogs', entry)
             }
         }
     }
