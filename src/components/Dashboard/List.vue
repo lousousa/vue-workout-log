dd38faad04a61d2566ba9cedd7433de5b2de6a28
wizard <rumpertumpski@gmail.com>
2021-02-07 22:36:50 -0300

    add pagination

--- src/components/Dashboard/List.vue
+++ src/components/Dashboard/List.vue
@@ -1,12 +1,20 @@
 <template lang="pug">
     div
-        .p-4(v-if='! logs.length')
+        .p-4(v-if='! list.length')
             p.text-center.text-xs.text-gray-600 You have no log.
         .p-4(v-else)
             .border.rounded
                 .w-full.text-center.text-xs.p-2.text-gray-400.border-b
                     h1 LOGS
-                .flex.flex-wrap.items-center(v-for='log, idx in logs' :key='idx')
+                .flex.flex-wrap.items-center.bg-gray-100.border-b
+                    .text-center.p-2(class='w-1/4')
+                        p.text-xs.text-gray-600.font-bold Date
+                    .text-center.p-2(class='w-1/4')
+                        p.text-xs.text-gray-600.font-bold Activity
+                    .text-center.p-2(class='w-1/4')
+                        p.text-xs.text-gray-600.font-bold Time
+                    .text-center.p-2(class='w-1/4')
+                .flex.flex-wrap.items-center(v-for='log, idx in getPaginatedList()' :key='idx')
                     .text-center.p-2(class='w-1/4')
                         p.text-xs.text-gray-600 {{ log.date }}
                     .text-center.p-2(class='w-1/4')
@@ -16,23 +24,38 @@
                     .text-center.p-2(class='w-1/4')
                         a(href='javascript:;' @click='removeItem(idx)')
                             i.jam.jam-trash.text-gray-600
+                .text-center.items-center.bg-gray-100.border-t.p-2
+                    a.mx-2.text-xs.text-green-600.font-bold(v-for='page, idx in getPagesArray()' :key='idx' href='javascript:;' @click='pagination.currentPage = page') {{ page }}
                 .w-full.text-center.text-xs.p-2.text-gray-600.border-t
-                    p Total spent: <b>{{ logs.map(log => log.minutes).reduce((sum, val) => sum + val) }} min</b>
+                    p Total spent: <b>{{ list.map(log => log.minutes).reduce((sum, val) => sum + val) }} min</b>
 </template>
 
 <script>
     export default {
         created () {
-            this.logs = this.$store.state.logs
+            this.list = this.$store.state.logs
         },
         data () {
             return {
-                logs: []
+                list: [],
+                pagination: {
+                    currentPage: 1,
+                    perPage: 2
+                }
             }
         },
         methods: {
             removeItem (idx) {
                 this.$store.commit('removeFromLogs', idx)
+            },
+            getPaginatedList () {
+                let start = (this.pagination.currentPage - 1) * this.pagination.perPage
+                return this.list.slice(start, start + this.pagination.perPage)
+            },
+            getPagesArray () {
+                let pages = []
+                for (let i = 1; i <= Math.ceil(this.list.length / this.pagination.perPage); i++) pages.push(i)
+                return pages
             }
         }
     }
